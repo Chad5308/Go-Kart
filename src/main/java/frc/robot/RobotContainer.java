e2af224f2987f4d87358a388830b0ed74541f622
Chad <Chad25309@gmail.com>
2024-03-28 22:14:16 -0400

    should be done

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,28 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.DriveCommand;
+import frc.robot.subsystems.Drive;
 
 public class RobotContainer {
+public Drive drive = new Drive();
+public DriveCommand driveCommand = new DriveCommand(drive);
+
   public RobotContainer() {
+    drive.setDefaultCommand(driveCommand);
+
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+
+
+  private void configureBindings() {
+
+driveCommand.controller.x().onTrue(Commands.runOnce(() -> {driveCommand.inPlace();}));
+
+
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
--- src/main/java/frc/robot/commands/DriveCommand.java
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -1,7 +1,31 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.Drive;
 
 public class DriveCommand extends Command{
+
+    public Drive drive;
+    public CommandXboxController controller;
+    public boolean inPlace = false;
+
+    public DriveCommand(Drive drive){
+        this.drive = drive;
+        controller = new CommandXboxController(0);
+    }
+
+
+    @Override
+    public void execute(){
+        drive.drive(controller.getLeftY(), controller.getRightX(), inPlace);
+    }
+
+
+    public void inPlace(){
+      inPlace = !inPlace;
+    }
+
+
     
 }
